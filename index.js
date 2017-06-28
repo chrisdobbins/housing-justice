cb51497c701c669bfefb9bf84c3028df44d67ccd
christopher dobbins <christopherjosephdobbins@gmail.com>
2017-06-28 04:06:28 -0400

    added csv file writing and some data cleaning

--- index.js
+++ index.js
@@ -3,50 +3,66 @@
 const requestPromise = require('request-promise'),
       cheerio = require('cheerio'),
       fs = require('fs'),
-      createCsvWriter = require('csv-writer').createArrayCsvWriter,
+      csv = require('fast-csv'),
       path = './data.csv';
 
+     const stream = fs.createWriteStream(path);
 
 function main() {
+    // hardcoding this for now
+    //
+    let parcelNumber = 1400050323;
   const options = {
     uri: 'http://qpublic9.qpublic.net/ga_display_dw.php?county=ga_fulton&KEY=14%20001400050323',
     headers: {
       'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0'
-    }	  
+    }
   };
   requestPromise(options).then(data => {
     let $ = cheerio.load(data);
-    let targetTable = $('tbody', '.table_class').eq(-3);
-    let vals = "";
-    let csvHeader = [];
+    let targetTable = $('tbody', '.table_class').eq(-3); 
+    let textToWrite = [];
    // not using arrow fn here because of the
-   // change in `this`
-    targetTable.children().each(function(idx){
-      if (idx == 1) {
-        csvHeader = $(this).text().trim().replace(/\n/g, ',').split(',');              
+   // change in the context of `this`
+    targetTable.children().first().siblings().each(function(idx){
+      if (idx == 0) {
+        // TODO: trim each element of this
+       let tableHeader = makeSemicolonSeparated($(this).text()).split(';');
+       // taking advantage of the empty element at beginning of array
+       tableHeader.unshift('Parcel Number');
+       textToWrite.push(tableHeader);
       } else {
-        console.log($(this).text());               
-        
-      } 
-      initCsv(csvHeader, path);
+        // TODO: clean this input
+        let tableRowText = makeSemicolonSeparated($(this).text());
+        let tableRow = tableRowText.split(';');
+        let csvRow = tableRow.map((currVal) => {
+            let isDollarAmount = currVal.match(/[\$]/);
+            if (isDollarAmount) {
+              return currVal.replace(/\$/, '').replace(',', '').trim();
+            }
+            return currVal;
+        });
+
+        // taking advantage of the empty element at beginning of array
+        csvRow.unshift(parcelNumber);
+        textToWrite.push(csvRow);
+      }
     });
+     csv.writeToStream(stream, textToWrite, {headers: true});
+      console.log(textToWrite);
   }, (err) => {
     console.log('scraping failed: ', err);
-  });	
+  });
 }
 
-function initCsv(header, path) {
-  const opts = {
-    header: header,
-    path: path
-  };
-   createCsvWriter(opts);
+// opting for semicolons instead of commas due to
+// commas in dollar figures
+function makeSemicolonSeparated(data) {
+  return data.replace(/[\n]/g, ';');
 }
+
 
 function writeCsvRow(record) {
-  csvWriter.writeRecords(record).then( () => {
-    console.log('yay!');
-  });
 }
 
 main();
